1f816b2d685782c2f8b6a63b4603a4793462cde9
Benedict Bode <benedict.bode11@gmail.com>
2023-12-03 11:08:00 +0100

    added basic functions

--- index.js
+++ index.js
@@ -5,12 +5,34 @@ const fs = require('fs'); // for file system operations
 const path = require('path'); // for handling file paths
 
 const app = express();
-const upload = multer({ dest: 'uploads/' }); // configure multer
 
 // Mock data
 let messages = [{"message_type":"quote_offer","text":"Hey, I would love to work for you. my mail is benedict.bode11@gmail.com and my phone number is +4915757778208 and my website is https:\/\/www.google.com just leave a quick message but my price is $ 1500","created_at":"2023-12-03T09:26:09Z","sender_type":"service_provider","attachments":[],"conversation_id":1,"id":1},{"message_type":"quote_offer","conversation_id":2,"sender_type":"service_provider","created_at":"2023-12-03T09:26:09Z","id":1,"text":"Hey I have a great offer for you! I would fix your broken toilet for only 100000$ 😇","attachments":[]}]
 let conversations = [{"service_provider_name":"Colton Ruecker","id":1,"updated_at":"2023-12-03T09:26:48Z","state":"quoted","created_at":"2023-12-03T09:26:48Z","customer_name":"Reyes Herzog"},{"customer_name":"Alycia Homenick","id":2,"created_at":"2023-12-03T09:26:48Z","updated_at":"2023-12-03T09:26:48Z","state":"quoted","service_provider_name":"Ampara West"}]
 // Routes
+
+app.use(express.json());
+
+// Set up storage engine
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const { conversation_id, message_id } = req.params;
+        const dir = `./uploads/${conversation_id}/${message_id}`;
+
+        // Create directory if it doesn't exist
+        fs.mkdirSync(dir, { recursive: true });
+
+        cb(null, dir);
+    },
+    filename: function (req, file, cb) {
+        // You can access the original filename of the file using `file.originalname`
+        // and set the filename as you wish. Here, I'm keeping the original filename.
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
 app.get('/conversations', (req, res) => {
   res.json(conversations);
 });
@@ -19,12 +41,50 @@ app.get('/messages', (req, res) => {
   res.json(messages);
 });
 
+app.post('/messages', (req, res) => {
+  const message = req.body; // Get the message from the request body
+  messages.push(message); // Add the message to the array
+  res.json(message); // Send the message back in the response
+});
+
+app.put('/messages', (req, res) => {
+  const newMessageData = req.body;
+  const id = newMessageData.id;
+  const conversation_id = newMessageData.conversation_id;
+  const messageIndex = messages.findIndex(m => m.id === id && m.conversation_id === conversation_id);
+
+  if (messageIndex === -1) {
+    return res.status(404).send('Message not found');
+  }
+
+  messages[messageIndex] = { ...messages[messageIndex], ...newMessageData };
+  res.json(messages[messageIndex]);
+});
+
+app.put('/conversations', (req, res) => {
+  const newConversationData = req.body;
+  const id = newConversationData.id;
+  const conversationIndex = conversations.findIndex(c => c.id === id);
+
+  if (conversationIndex === -1) {
+    return res.status(404).send('Conversation not found');
+  }
+
+  conversations[conversationIndex] = { ...conversations[conversationIndex], ...newConversationData };
+  res.json(conversations[conversationIndex]);
+});
+
+
 // ... other routes ...
 
 // File upload route
 app.post('/upload/:conversation_id/:message_id', upload.single('file'), (req, res) => {
-  // Handle file upload
-  // File is available in req.file
+  if (!req.file) {
+    console.log('No file received');
+    return res.status(400).send('No file uploaded.');
+  }
+
+  console.log('File received:', req.file);
   res.status(200).send('File uploaded');
 });
 
